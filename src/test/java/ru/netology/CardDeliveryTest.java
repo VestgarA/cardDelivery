d7a67ddaba1c9fb7313045ee58f6b4a04dad5135
vestgar <sid06@bk.ru>
2025-08-15 22:14:47 +0300

    taska №2

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.lang.Object;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
+import static java.awt.SystemColor.menu;
 import static java.lang.String.format;
 import static java.time.LocalDate.now;
 
@@ -21,7 +23,11 @@ public class CardDeliveryTest {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     String dateMeeting = date.format(formatter);
 
+    LocalDate dateWeek = LocalDate.now().plusWeeks(1);
+    DateTimeFormatter formatt = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    String dateWeekMeeting = dateWeek.format(formatter);
 
+    //Задача №1: заказ доставки карты
     @Test
     public void successfulAppointmentBooking() {
         Selenide.open("http://localhost:9999");
@@ -150,5 +156,39 @@ public class CardDeliveryTest {
         $$("button").find(Condition.text("Забронировать")).click();
         $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition
                 .text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+// Задача №2: взаимодействие с комплексными элементами
+
+    @Test
+    public void selectionCityFromList() {
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Мо");
+        $(".popup .menu :nth-child(3)").click();
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(dateMeeting);
+        $("[data-test-id='name'] input").setValue("Иван Иванов");
+        $("[data-test-id='phone'] input").setValue("+79500000000");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.text("Забронировать")).click();
+        $(Selectors.withText("Встреча успешно забронирована на"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void ChoosingDateForWeekAhead() {
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(dateWeekMeeting);
+        $("[data-test-id='name'] input").setValue("Иван Иванов");
+        $("[data-test-id='phone'] input").setValue("+79500000000");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.text("Забронировать")).click();
+        $(Selectors.withText("Встреча успешно забронирована на"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+
     }
 }
